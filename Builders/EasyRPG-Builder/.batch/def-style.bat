7dba15e467d92c7c7e97b19b4acd3c86d5eaac7a
DRok17 <81541725+DRok17@users.noreply.github.com>
2022-05-19 22:12:24 -0400

    EB: Fixed AD0 issue

--- Builders/EasyRPG-Builder/.batch/def-style.bat
+++ Builders/EasyRPG-Builder/.batch/def-style.bat
@@ -35,74 +35,129 @@ IF %M%==6 GOTO NONE
 :NONE
 @echo off
 cd .
-(echo template-nostartup) > ../contents/template.txt
+copy "..\contents\template.xml" "."
+ren template.xml template-old.xml
+copy "..\..\..\.batch\template-nostartup.xml" "."
+ren template-nostartup.xml template.xml
+copy ".\template.xml" "..\contents\"
 (echo ad0) > z-style-NOW.txt
 timeout 2 >nul
+del ".\template.xml"
 GOTO MENU
 
 :CENT
 @echo off
 cd .
-(echo template) > ../contents/template.txt
+IF EXIST .\template-old.xml (
+    ren template-old.xml template.xml
+    del "..\contents\template.xml"
+    copy ".\template.xml" "..\contents\"
+    GOTO NEXTCENT
+  ) ELSE (
+    GOTO NEXTCENT
+  )
+:NEXTCENT
 (echo a1) > z-lastyle-new.txt
 set /p stynew=< .\z-lastyle-new.txt
 cd .
 start def-style-OUT.bat
 timeout 3 >nul
+@echo off
 @echo|set /p="%stynew%"> "./z-lastyle-cur.txt"
 @echo|set /p="%stynew%"> "./z-style-NOW.txt"
+del ".\template.xml"
 GOTO MENU
 
 :CENTUP
 @echo off
 cd .
-(echo template) > ../contents/template.txt
+IF EXIST .\template-old.xml (
+    ren template-old.xml template.xml
+    del "..\contents\template.xml"
+    copy ".\template.xml" "..\contents\"
+    GOTO NEXTCENTUP
+  ) ELSE (
+    GOTO NEXTCENTUP
+  )
+:NEXTCENTUP
 (echo a2) > z-lastyle-new.txt
 set /p stynew=< .\z-lastyle-new.txt
 cd .
 start def-style-OUT.bat
 timeout 3 >nul
+@echo off
 @echo|set /p="%stynew%"> "./z-lastyle-cur.txt"
 @echo|set /p="%stynew%"> "./z-style-NOW.txt"
+del ".\template.xml"
 GOTO MENU
 
 :RIGH
 @echo off
 cd .
-(echo template) > ../contents/template.txt
+IF EXIST .\template-old.xml (
+    ren template-old.xml template.xml
+    del "..\contents\template.xml"
+    copy ".\template.xml" "..\contents\"
+    GOTO NEXTRIGH
+  ) ELSE (
+    GOTO NEXTRIGH
+  )
+:NEXTRIGH
 (echo psmobile) > z-lastyle-new.txt
 set /p stynew=< .\z-lastyle-new.txt
 cd .
 start def-style-OUT.bat
 timeout 3 >nul
+@echo off
 @echo|set /p="%stynew%"> "./z-lastyle-cur.txt"
 @echo|set /p="%stynew%"> "./z-style-NOW.txt"
+del ".\template.xml"
 GOTO MENU
 
 :RIGHUP
 @echo off
 cd .
-(echo template) > ../contents/template.txt
+IF EXIST .\template-old.xml (
+    ren template-old.xml template.xml
+    del "..\contents\template.xml"
+    copy ".\template.xml" "..\contents\"
+    GOTO NEXTRIGHUP
+  ) ELSE (
+    GOTO NEXTRIGHUP
+  )
+:NEXTRIGHUP
 (echo a5) > z-lastyle-new.txt
 set /p stynew=< .\z-lastyle-new.txt
 cd .
 start def-style-OUT.bat
 timeout 3 >nul
+@echo off
 @echo|set /p="%stynew%"> "./z-lastyle-cur.txt"
 @echo|set /p="%stynew%"> "./z-style-NOW.txt"
+del ".\template.xml"
 GOTO MENU
 
 :RIGHLOW
 @echo off
 cd .
-(echo template) > ../contents/template.txt
+IF EXIST .\template-old.xml (
+    ren template-old.xml template.xml
+    del "..\contents\template.xml"
+    copy ".\template.xml" "..\contents\"
+    GOTO NEXTRIGHLOW
+  ) ELSE (
+    GOTO NEXTRIGHLOW
+  )
+:NEXTRIGHLOW
 (echo vd) > z-lastyle-new.txt
 set /p stynew=< .\z-lastyle-new.txt
 cd .
 start def-style-OUT.bat
 timeout 3 >nul
+@echo off
 @echo|set /p="%stynew%"> "./z-lastyle-cur.txt"
 @echo|set /p="%stynew%"> "./z-style-NOW.txt"
+del ".\template.xml"
 GOTO MENU
 
 :EOF
